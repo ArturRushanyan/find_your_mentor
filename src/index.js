0693e56e14b9cd5cee0d55babad1f41a62dc4841
Artur <rushanyan-artur@mail.ru>
2022-06-24 12:22:57 +0400

    Added helping tools

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const logger  = require('morgan');
+
 const config = require('./config');
 
 const app = express();
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.listen(config.port, () => {
     console.log(`Server is up on port: ${config.port}`);
