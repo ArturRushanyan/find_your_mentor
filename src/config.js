3b217690cfc05cc316d0cd620025153e5cdbe390
Artur <rushanyan-artur@mail.ru>
2022-06-24 12:37:07 +0400

    Connected DB "mongodb"

--- src/config.js
+++ src/config.js
@@ -1,7 +1,14 @@
 require('dotenv').config()
 
+const db_host = process.env.DB_HOST || 'localhost';
+const db_port = process.env.DB_PORT || 27017;
+const db_name = process.env.DB_NAME || 'db_mentorly';
+
 const config = {
     port: process.env.RUNNING_PORT || 3000,
+    DB: {
+        url: `mongodb://${db_host}:${db_port}/${db_name}`,
+    },
 };
 
 module.exports = config;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const logger  = require('morgan');
+const mongoose =  require('mongoose');
 
 const config = require('./config');
 
@@ -8,6 +9,16 @@ const app = express();
 app.use(logger('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+mongoose.connect(config.DB.url, {
+    useNewUrlParser: true,
+}).then(() => {
+    console.log('Successfully connected to the database')
+}).catch((err) => {
+    console.log('Could not connect to the database. Exiting now...');
+    console.log('err ', err);
+    process.exit();
+});
 
 app.listen(config.port, () => {
     console.log(`Server is up on port: ${config.port}`);
